fbb4d380d1e13f3fad3079e5e47fa6da4061b0ca
Douglas Araújo <devdoug45@gmail.com>
2023-09-04 00:04:22 -0300

    test: cria os testes do controller de produtos

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -2,8 +2,12 @@ const { productsService } = require('../services');
 const { mapHTTPStatus } = require('../utils/mapStatusHTTP');
 
 const getAllProducts = async (_req, res) => {
-  const { status, data } = await productsService.getAllProducts();
-  return res.status(mapHTTPStatus(status)).json(data);
+  try {
+    const { status, data } = await productsService.getAllProducts();
+    return res.status(mapHTTPStatus(status)).json(data);
+  } catch (error) {
+    return res.status(mapHTTPStatus(error)).send({ message: 'Internal Server Error' });
+  }
 };
 
 const getProductById = async (req, res) => {
---
+++ backend/tests/unit/controllers/productsController.test.js
@@ -0,0 +1,73 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const { productsService } = require('../../../src/services');
+const { productsController } = require('../../../src/controllers');
+const {
+  productsFromModel,
+} = require('../../mocks/models/productsMocks');
+
+describe('Testa o controller de produtos', function () {
+  it('Retorna um status de SUCCESSFUL e os produtos', async function () {
+    sinon.stub(productsService, 'getAllProducts').resolves({
+      status: 'SUCCESSFUL',
+      data: productsFromModel,
+    });
+
+    const req = {};
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.getAllProducts(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(productsFromModel);
+  });
+
+  it('Retorna um status de INTERNAL_SERVER_ERROR', async function () {
+    sinon.stub(productsService, 'getAllProducts').resolves({
+      status: undefined,
+      data: undefined,
+    });
+
+    const req = {};
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      send: sinon.stub(),
+    };
+
+    await productsController.getAllProducts(req, res);
+    expect(res.status).to.have.been.calledWith(500);
+    expect(res.send).to.have.been.calledWith({
+      message: 'Internal Server Error',
+    });
+  });
+
+  it('Retorna um status de SUCCESSFUL e o produto por id', async function () {
+    sinon.stub(productsService, 'getProductById').resolves(productsFromModel[0]);
+
+    const req = {
+      params: { id: 1 },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.getProductById(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(productsFromModel[0]);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
